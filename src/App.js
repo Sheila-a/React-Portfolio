128eff97367151b5213d64d34499d6d06af92eb2
Sheila-a <ugwuodo20@gmail.com>
2022-05-21 17:37:00 +0100

    Made other paths of site visible, while stile editing queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Intro from "./Components/Intro/intro";
 import About from "./Components/About/about";
-// import ProductList from "./Components/Product_list/product_list";
+import ProductList from "./Components/Product_list/product_list";
 // import Skills from "./Components/My skills/skills";
 // import Contact from "./Components/Contact/contact";
 // import Footer from "./Components/Footer/footer";
@@ -10,7 +10,7 @@ const App = () => {
     <div>
       <Intro />
       <About />
-      {/* <ProductList /> */}
+      <ProductList />
       {/* <Skills /> */}
       {/* <Contact /> */}
       {/* <Footer /> */}
