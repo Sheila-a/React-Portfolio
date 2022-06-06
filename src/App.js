76aadfd3cb1f9e427a28f80cbf1912d27c453d0b
Sheila-a <ugwuodo20@gmail.com>
2022-06-06 11:02:32 +0100

    Updated for Navbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import ProductList from "./Components/Product_list/product_list";
 import Skills from "./Components/My skills/skills";
 import Contact from "./Components/Contact/contact";
 import Footer from "./Components/Footer/footer";
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Intro />
       <About />
       <ProductList />
